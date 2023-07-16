04e9807b109ebf547313c4ca888d4892c0c7ffcb
Thiago <drthiagodf@gmail.com>
2023-07-16 09:21:24 -0300

    melhorando codigo

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String nomeCliente = "Fulano de Tal";
+        String tipoConta = "Corrente";
+        double saldoAtualizado = 2570.99;
+        int opcaoDesejada = 0;
 
-        System.out.println("""
-                ***********************
-                 Dados iniciais do cliente:
-                 
-                 Nome: Thiago Soares
-                 Tipo conta: Corrente
-                 Saldo inicial: R$ 2500,00
-                 ***********************
-                 """);
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nomeCliente);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldoAtualizado);
+        System.out.println("\n***********************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Receber valor
+                3 - Transferir valor
+                4 - Sair
+                
+                """;
 
         Scanner leitura = new Scanner(System.in);
-        int opcaoDesejada = 0;
-        double saldoAtualizado =  2500.00;
 
         while (opcaoDesejada != 4) {
-            System.out.println("""
-                 
-                 Operações
-                 1- Consultar saldos
-                 2- Receber valor
-                 3- Transferir valor
-                 4- Sair
-                 
-                 Digite a opção desejada:""");
+            System.out.println(menu);
             opcaoDesejada = leitura.nextInt();
+
             if (opcaoDesejada == 1){
                 System.out.println("Saldo Atualizado = R$" + saldoAtualizado);
-            }
-            if (opcaoDesejada == 2){
-                System.out.println("Informe o valor a receber:");
+            } else if (opcaoDesejada == 2) {
+                System.out.println("Informe o valor recebido: ");
                 double valorRecebido = leitura.nextDouble();
-                saldoAtualizado = saldoAtualizado + valorRecebido;
+                saldoAtualizado += valorRecebido;
                 System.out.println("Saldo Atualizado = " + saldoAtualizado);
-            }
-            if (opcaoDesejada == 3){
-                System.out.println("Informe o valor que deseja transferir:");
+            } else if (opcaoDesejada == 3){
+                System.out.println("Informe o valor que deseja transferir: ");
                 double valorTransferido = leitura.nextDouble();
                 if (valorTransferido > saldoAtualizado){
                     System.out.println("Não há saldo suficiente para fazer essa transferência");
                     System.out.println("Saldo Atualizado = " + saldoAtualizado);
                 } else {
-                    saldoAtualizado = saldoAtualizado - valorTransferido;
+                    saldoAtualizado -= valorTransferido;
                     System.out.println("Saldo Atualizado = " + saldoAtualizado);
                 }
-            }
-
-            if (opcaoDesejada < 1 || opcaoDesejada > 4){
+            } else if (opcaoDesejada != 4) {
                 System.out.println("Opção Inválida");
             }
         }
